712a1452edc3914130713657d5d59d04f2fefaec
Alam Hosen <alamhosen.web@gmail.com>
2022-04-03 23:56:17 +0600

    nav complete

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div className='home-container'>
+            <div className='header-area'>
+                
+            </div>
+            <div className='review-area'></div>
+        </div>
+    );
+};
+
+export default Home;
